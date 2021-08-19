914dd21fcceaacc37eabe0bd976572466488dbcf
JackTPatterson <jtyler03@optonline.net>
2021-08-19 16:41:54 -0400

    added animations

--- users/static/users/js/index.js
+++ users/static/users/js/index.js
@@ -47,6 +47,7 @@
 	});
 })(jQuery);
 
+
 $(function() {
 	if ($(window).width() < 440) {
 		$('#divider1').css({'display': 'none'});
@@ -62,3 +63,52 @@ abt.onclick = function () {
 	}, 1300);
 }
 
+var images = [
+    'https://cybersandbox.ca/resources/new-like.png',
+    'https://cybersandbox.ca/resources/new-heart.png'
+];
+
+
+function createParticle (x, y) {
+
+	const particle = document.createElement('particle');
+	document.querySelector('#button').appendChild(particle);
+  
+	// Calculate a random size from 10px to 30px
+	const size = Math.floor(Math.random() * 20 + 10);
+	particle.style.width = `${size}px`;
+	particle.style.height = `${size}px`;
+	particle.style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
+  
+	// Generate a random x & y destination within a distance of 75px from the mouse
+	const destinationX = x + (Math.random() - 0.5) * 2 * 90;
+	const destinationY = y + (Math.random() - 0.5) * 2 * 90;
+	// Store the animation in a variable because we will need it later
+	const animation = particle.animate([
+	  {
+		// Set the origin position of the particle
+		// We offset the particle with half its size to center it around the mouse
+		transform: `translate(${x - (size / 2)}px, ${y - (size / 2)}px)`,
+		opacity: 1
+	  },
+	  {
+		opacity: 1
+	  },
+	  {
+		// We define the final coordinates as the second keyframe
+		transform: `translate(${destinationX}px, ${destinationY}px)`,
+		opacity: 0
+	  }
+	], {
+	  // Set a random duration from 1000 to 2000ms
+	  duration: 1000 + Math.random() * 1000,
+	  easing: 'cubic-bezier(0, .9, .57, 1)',
+	  // Delay every particle with a random value from 0ms to 200ms
+	  delay: Math.random() * 200
+	});
+	
+	// When the animation is complete, remove the element from the DOM
+	animation.onfinish = () => {
+	  particle.remove();
+	};
+  }
